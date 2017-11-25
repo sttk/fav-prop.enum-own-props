6d66da1a2a9a2d871c402f31e0f1e5d328fe892e
sttk <sttk.xslet@gmail.com>
2017-11-25 12:21:49 +0900

    Modify tests to use .to.have.deep.members instead of .to.deep.equal for comparing arrays.

--- test/enum-own-props.test.js
+++ test/enum-own-props.test.js
@@ -10,8 +10,8 @@ var enumOwnProps = fav.prop.enumOwnProps;
 describe('fav.prop.enumOwnProps', function() {
 
   it('Should get all props when the argument is a plain object', function() {
-    expect(enumOwnProps({})).to.deep.equal([]);
-    expect(enumOwnProps({ a: 1, b: true, c: 'C' }).sort()).to.deep.equal([
+    expect(enumOwnProps({})).to.have.deep.members([]);
+    expect(enumOwnProps({ a: 1, b: true, c: 'C' })).to.have.deep.members([
       { key: 'a', value: 1 },
       { key: 'b', value: true },
       { key: 'c', value: 'C' }
@@ -21,14 +21,14 @@ describe('fav.prop.enumOwnProps', function() {
   it('Should not get properties of prototype', function() {
     function Fn0() {}
     Fn0.prototype.a = 1;
-    expect(enumOwnProps(new Fn0())).to.deep.equal([]);
+    expect(enumOwnProps(new Fn0())).to.have.deep.members([]);
     function Fn1() {
       this.b = true;
       this.c = 'C';
     }
     Fn1.prototype = new Fn0();
     Fn1.prototype.d = 'D';
-    expect(enumOwnProps(new Fn1()).sort()).to.deep.equal([
+    expect(enumOwnProps(new Fn1())).to.have.deep.members([
       { key: 'b', value: true },
       { key: 'c', value: 'C' },
     ]);
@@ -41,26 +41,26 @@ describe('fav.prop.enumOwnProps', function() {
       b: { value: true },
       c: { value: 'C' },
     });
-    expect(enumOwnProps(obj)).to.deep.equal([{ key: 'a', value: 1 }]);
+    expect(enumOwnProps(obj)).to.have.deep.members([{ key: 'a', value: 1 }]);
   });
 
   it('Should return an empty array when the argument is nullish', function() {
-    expect(enumOwnProps(undefined)).to.deep.equal([]);
-    expect(enumOwnProps(null)).to.deep.equal([]);
+    expect(enumOwnProps(undefined)).to.have.deep.members([]);
+    expect(enumOwnProps(null)).to.have.deep.members([]);
   });
 
   it('Should return an empty array when the argument is primitive type',
   function() {
-    expect(enumOwnProps(true)).to.deep.equal([]);
-    expect(enumOwnProps(false)).to.deep.equal([]);
-    expect(enumOwnProps(0)).to.deep.equal([]);
-    expect(enumOwnProps(123)).to.deep.equal([]);
+    expect(enumOwnProps(true)).to.have.deep.members([]);
+    expect(enumOwnProps(false)).to.have.deep.members([]);
+    expect(enumOwnProps(0)).to.have.deep.members([]);
+    expect(enumOwnProps(123)).to.have.deep.members([]);
   });
 
   it('Should return an array having index strings as keys when the argument' +
   '\n\tis a string', function() {
-    expect(enumOwnProps('')).to.deep.equal([]);
-    expect(enumOwnProps('abc')).to.deep.equal([
+    expect(enumOwnProps('')).to.have.deep.members([]);
+    expect(enumOwnProps('abc')).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
@@ -73,7 +73,7 @@ describe('fav.prop.enumOwnProps', function() {
       // Throw TypeError on Node.js version 0.11 or later.
       //console.error('\t', e.message);
     }
-    expect(enumOwnProps(s)).to.deep.equal([
+    expect(enumOwnProps(s)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
@@ -85,7 +85,7 @@ describe('fav.prop.enumOwnProps', function() {
       // Throw TypeError on Node.js version 0.11 or later.
       //console.error('\t', e.message);
     }
-    expect(enumOwnProps(s)).to.deep.equal([
+    expect(enumOwnProps(s)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
@@ -95,14 +95,14 @@ describe('fav.prop.enumOwnProps', function() {
   it('Should return an array of index strings as keys when the argument is' +
   '\n\ta String object', function() {
     var s = new String('abc');
-    expect(enumOwnProps(s).sort()).to.deep.equal([
+    expect(enumOwnProps(s)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
     ]);
 
     s.aaa = 'AAA';
-    expect(enumOwnProps(s).sort()).to.deep.equal([
+    expect(enumOwnProps(s)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
@@ -110,7 +110,7 @@ describe('fav.prop.enumOwnProps', function() {
     ]);
 
     Object.defineProperty(s, 'bbb', { value: 'BBB' });
-    expect(enumOwnProps(s).sort()).to.deep.equal([
+    expect(enumOwnProps(s)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: '2', value: 'c' },
@@ -120,8 +120,8 @@ describe('fav.prop.enumOwnProps', function() {
 
   it('Should return an array of index strings as keys when the argument is' +
   '\n\ta array', function() {
-    expect(enumOwnProps([])).to.deep.equal([]);
-    expect(enumOwnProps([1, 2, 3]).sort()).to.deep.equal([
+    expect(enumOwnProps([])).to.have.deep.members([]);
+    expect(enumOwnProps([1, 2, 3])).to.have.deep.members([
       { key: '0', value: 1 },
       { key: '1', value: 2 },
       { key: '2', value: 3 },
@@ -129,14 +129,14 @@ describe('fav.prop.enumOwnProps', function() {
 
     var a = ['a', 'b'];
     a.aaa = 'AAA';
-    expect(enumOwnProps(a).sort()).to.deep.equal([
+    expect(enumOwnProps(a)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: 'aaa', value: 'AAA' },
     ]);
 
     Object.defineProperty(a, 'bbb', { value: 'BBB' });
-    expect(enumOwnProps(a).sort()).to.deep.equal([
+    expect(enumOwnProps(a)).to.have.deep.members([
       { key: '0', value: 'a' },
       { key: '1', value: 'b' },
       { key: 'aaa', value: 'AAA' },
@@ -146,15 +146,15 @@ describe('fav.prop.enumOwnProps', function() {
   it('Should return appended properties when the argument is a function',
   function() {
     var fn = function() {};
-    expect(enumOwnProps(fn)).to.deep.equal([]);
+    expect(enumOwnProps(fn)).to.have.deep.members([]);
 
     fn.aaa = 'AAA';
-    expect(enumOwnProps(fn)).to.deep.equal([
+    expect(enumOwnProps(fn)).to.have.deep.members([
       { key: 'aaa', value: 'AAA' },
     ]);
 
     Object.defineProperty(fn, 'bbb', { value: 'BBB' });
-    expect(enumOwnProps(fn).sort()).to.deep.equal([
+    expect(enumOwnProps(fn)).to.have.deep.members([
       { key: 'aaa', value: 'AAA' }
     ]);
   });
@@ -167,20 +167,20 @@ describe('fav.prop.enumOwnProps', function() {
     }
 
     var symbol = Symbol('foo');
-    expect(enumOwnProps(symbol)).to.deep.equal([]);
+    expect(enumOwnProps(symbol)).to.have.deep.members([]);
 
     try {
       symbol.aaa = 'AAA';
     } catch (e) {
       //console.error('\t', e.message);
     }
-    expect(enumOwnProps(symbol)).to.deep.equal([]);
+    expect(enumOwnProps(symbol)).to.have.deep.members([]);
 
     try {
       Object.defineProperty(symbol, 'bbb', { value: 'BBB' });
     } catch (e) {
       //console.error('\t', e.message);
     }
-    expect(enumOwnProps(symbol)).to.deep.equal([]);
+    expect(enumOwnProps(symbol)).to.have.deep.members([]);
   });
 });
